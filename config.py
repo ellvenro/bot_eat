68cf5faf22b8a959775092a59b49ba820e4ec303
ellvenro <lidiya.ellie@yandex.ru>
2021-07-20 23:39:29 +0300

    Добавление файлов

---
+++ config.py
@@ -0,0 +1,11 @@
+token = ''                                                          # токен бота, полученный через @BotFather
+
+API_key_search = ''                                                 # API-ключ поиска по организациям для Яндекс
+yandex_search = 'https://search-maps.yandex.ru/v1/'                 # сайт Яндекс, в котором ведется поиск по организациям
+
+API_key_geo = ''                                                    # API-ключ HTTP геокодер
+yandex_geo = 'https://geocode-maps.yandex.ru/1.x'                   # сайт Яндекс для запроса на геокодирование
+
+API_key_gis = ''                                                    # API-ключ для 2gis
+gis_search = 'https://catalog.api.2gis.com/3.0/items'               # get-запрос для Places API
+gis_geo = 'https://catalog.api.2gis.com/3.0/items/geocode'          # get-запрос для Geocoder API
