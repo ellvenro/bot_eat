fa6a5d7d15fa3162437c9beed1773a9edbe583da
ellvenro <lidiya.ellie@yandex.ru>
2021-07-21 12:52:32 +0300

    Модернизация

--- bot_eat.py
+++ bot_eat.py
@@ -9,11 +9,7 @@ from telebot import types
 
 # необходимые глобальные переменные
 point = ''                                                  # местоположение на карте (долгота и широта, разделенные ',')
-msg = {                                                     # сообщение, которые выводятся пользователю
-    'start' : 'Тут можно перекусить.. Что выберешь?)',
-    'loc' : 'Покажи на карте, где ты находишься\n\nНу а если ты в СПб, можешь ввести ближайшую станцию метро..',
-    'call' : 'Оо.. Есть еще места, где можно перекусить, можешь выбрать'
-}
+
 
 print('Бот запущен. Нажмите Ctrl+C для завершения')
 bot = telebot.TeleBot(config.token)
@@ -25,7 +21,7 @@ bot = telebot.TeleBot(config.token)
 def func_location(message):
     global point
     point = str(message.location.longitude) + ','+ str(message.location.latitude)
-    functions.func_inline_button(bot, message.chat.id, msg['start'])
+    functions.func_inline_button(bot, message.chat.id, functions.msg['start'])
 
 # функция обработки команды /start
     # создает клавиатуру с возможностью отправки местоположения
@@ -34,19 +30,20 @@ def func_start(message):
     keyboard = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
     button = types.KeyboardButton(text='Отправить местоположение', request_location=True)
     keyboard.add(button)
-    bot.send_message(message.chat.id, msg['loc'], reply_markup=keyboard)
+    bot.send_message(message.chat.id, functions.msg['loc'], reply_markup=keyboard)
 
 # функция обработки inline кнопок
     # запускает функцию поиска по организациям
     # вызывает функцию создания inline клавиатуры
 @bot.callback_query_handler(func=lambda call: True)
 def func_call(call):
-    if (call.data == 'McDonald’s' or call.data == 'KFC' or call.data == 'Subway' or call.data == 'Burger King'):
-        if point == '':
-            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=msg['loc'])
-        else:
-            functions.func_search_gis(bot, call, point)
-            functions.func_inline_button(bot, call.message.chat.id, msg['call'])
+    if point == '':
+        bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.message_id)
+        bot.send_message(chat_id=call.message.chat.id, text=functions.msg['loc'])
+    elif (call.data == 'McDonald’s' or call.data == 'KFC' or call.data == 'Subway' or call.data == 'Burger King'):
+        functions.func_search_gis(bot, call, point)
+    elif call.data == 'var':
+        functions.func_var(bot, call)
 
 # функция обработки текста
     # определяет геопозицию введенного пользователем адреса (в частности станции метро)
@@ -55,7 +52,7 @@ def func_call(call):
 def func_text(message):
     global point
     point = functions.func_geo_gis(bot, message)
-    functions.func_inline_button(bot, message.chat.id, msg['start'])
+    functions.func_inline_button(bot, message.chat.id, functions.msg['start'])
 
 bot.polling()
 #if __name__ == '__main__':
--- functions.py
+++ functions.py
@@ -6,9 +6,19 @@ import requests
 
 from telebot import types
 
+# необходимые глобальные переменные
+result = ''                                                 # результат, приходящий через get-запрос, приведенный к словарю
+len = 0                                                     # количество найденных заведений
+cnt = 0                                                     # номер найденного заведения
+msg = {                                                     # сообщение, которые выводятся пользователю
+    'start' : 'Тут можно перекусить.. Что выберешь?)',
+    'loc' : 'Покажи на карте, где ты находишься\n\nНу а если ты в СПб, можешь ввести ближайшую станцию метро..',
+    'call' : 'Оо.. Есть еще места, где можно перекусить, можешь выбрать'
+}
+
 # функция определения геопозиции введенного пользователем адреса (в частности станции метро) с помощью 2gis
-def func_geo_gis(bot, message):
     # выполнение запроса к Geocoder API 2gis (get-запрос: gis_geo) с ключом API_key_gis
+def func_geo_gis(bot, message):
     query = 'СПб метро ' + message.text 
     r = requests.get(url=config.gis_geo, params={
         'q' : query,
@@ -21,8 +31,8 @@ def func_geo_gis(bot, message):
 
 # функция поиска по местам с помощью 2gis
     # работает с помощью библиотеки requests, запросы отправляются и принимаются согласно документации API поиска по организациям
-def func_search_gis(bot, call, point):
     # выполнение запроса Places API 2gis (get-запрос: gis_search) с ключом API_key_gis
+def func_search_gis(bot, call, point): 
     r = requests.get(url=config.gis_search, params={
         'q' : call.data,
         'key': config.API_key_gis,
@@ -31,22 +41,70 @@ def func_search_gis(bot, call, point):
         'fields' :'items.point,items.schedule',
         'radius' : '1000'
     }) 
+    global result
     result = json.loads(r.text)
+    func_output(bot, call)
 
-    try:
-        i = result['result']['total']
-        if i == 1:
-            text1 = 'Я нашел ' + str(i) + ' ресторан по твоему выбору'
-        elif (i > 1 and i < 5):
-            text1 = 'Я нашел ' + str(i) + ' ресторана по твоему выбору'
+# функция вывода на результатов поиска мест
+def func_output(bot, call):
+    if result['meta']['code'] == 200:
+        global len
+        len = result['result']['total']
+        if len == 1:
+            text1 = 'Я нашел ' + call.data + '! Но только ' + str(len) + ' ресторан'
+        elif (len > 1 and len < 5):
+            text1 = 'Я нашел ' + call.data + '! Даже ' + str(len) + ' ресторана'
         else:
-            text1 = 'Я нашел ' + str(i) + ' ресторанов по твоему выбору'
-        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=text1)
-        for item in result['result']['items']:
-            bot.send_venue(call.message.chat.id, item['point']['lat'], item['point']['lon'], item['name'], item['address_name'])
-    except:
+            text1 = 'Я нашел ' + call.data + '! Целых ' + str(len) + ' ресторанов'
+        bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.message_id)
+        bot.send_message(chat_id=call.message.chat.id, text=text1)
+
+        keyboard = types.InlineKeyboardMarkup()
+        if len > 1:
+            button = types.InlineKeyboardButton(text='Хочу другое место', callback_data='var')
+            keyboard.add(button)
+        button1 = types.InlineKeyboardButton(text='McDonald’s', callback_data='McDonald’s')
+        button2 = types.InlineKeyboardButton(text='KFC', callback_data='KFC')
+        keyboard.add(button1, button2)
+        button3 = types.InlineKeyboardButton(text='Subway', callback_data='Subway')  
+        button4 = types.InlineKeyboardButton(text='Burger King', callback_data='Burger King') 
+        keyboard.add(button3, button4)
+        bot.send_venue(chat_id=call.message.chat.id, 
+                       latitude=result['result']['items'][0]['point']['lat'], 
+                       longitude=result['result']['items'][0]['point']['lon'], 
+                       title=result['result']['items'][0]['name'], 
+                       address=result['result']['items'][0]['address_name'],
+                       reply_markup=keyboard)
+    else:
         text1 = 'Ресторанов '+ call.data + ' рядом нет..('
-        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=text1)
+        bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.message_id)
+        bot.send_message(chat_id=call.message.chat.id, text=text1)
+        func_inline_button(bot, call.message.chat.id, msg['call'])
+
+# функция смены найденной локации на другую
+    # происходит перебот элементов в списке с счетчиком cnt
+def func_var(bot, call):
+    global cnt
+    if cnt < len - 1:
+        cnt = cnt + 1
+    elif cnt == len - 1:
+        cnt = 0
+    keyboard = types.InlineKeyboardMarkup()
+    button = types.InlineKeyboardButton(text='Выбрать другое место', callback_data='var')
+    keyboard.add(button)
+    button1 = types.InlineKeyboardButton(text='McDonald’s', callback_data='McDonald’s')
+    button2 = types.InlineKeyboardButton(text='KFC', callback_data='KFC')
+    keyboard.add(button1, button2)
+    button3 = types.InlineKeyboardButton(text='Subway', callback_data='Subway')  
+    button4 = types.InlineKeyboardButton(text='Burger King', callback_data='Burger King') 
+    keyboard.add(button3, button4)
+    bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.message_id)
+    bot.send_venue(chat_id=call.message.chat.id, 
+                   latitude=result['result']['items'][cnt]['point']['lat'], 
+                   longitude=result['result']['items'][cnt]['point']['lon'], 
+                   title=result['result']['items'][cnt]['name'], 
+                   address=result['result']['items'][cnt]['address_name'],
+                   reply_markup=keyboard)
 
 # функция создания inline клавиатуры
 def func_inline_button(bot, c_id, text):
@@ -57,5 +115,4 @@ def func_inline_button(bot, c_id, text):
     button3 = types.InlineKeyboardButton(text='Subway', callback_data='Subway')  
     button4 = types.InlineKeyboardButton(text='Burger King', callback_data='Burger King') 
     keyboard.add(button3, button4)
-
     bot.send_message(c_id, text, reply_markup=keyboard)
